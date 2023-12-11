812c77f6d840e2759d5a5cbe37e1dafcccd41890
Robert Stapf <Robert.Stapf@flyeralarm.com>
2023-12-11 14:24:24 +0100

    test

---
+++ neuralNetwork.py
@@ -0,0 +1,20 @@
+import numpy
+import scipy.special
+import matplotlib.pyplot
+
+class neuralNetwork():
+
+    def __init__(self):
+        return
+
+    def train(self):
+        print("I'am training")
+    
+
+    def query(self):
+        print("I ask...")
+
+
+neuralNetwork = neuralNetwork()
+neuralNetwork.train()
+neuralNetwork.query()
