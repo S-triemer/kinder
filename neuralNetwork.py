02c148e9638dbace43ecd9f6b38bb8bc597e9853
jKiesmueller <johannes.kiesmueller@t-online.de>
2023-12-14 08:26:11 +0100

    Add neuralNetwork

---
+++ neuralNetwork.py
@@ -0,0 +1,25 @@
+#pip install numpy
+import numpy as np
+#pip install spicy  
+import scipy.special as scsp
+#pip install matplotlib
+import matplotlib.pyplot as matplotlib
+
+class neuralNetwork():
+    def __init__(self):
+        return
+    
+    def train(self):
+        print("I'am training")
+        pass
+
+    def query(self):
+        print("I ask...")
+        pass
+
+neuralNetwork = neuralNetwork()
+neuralNetwork.train()
+neuralNetwork.query()
+    
+print("performance = great")
+print(np.__version__)
