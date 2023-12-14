4f69a54d0915e651e6c9132e5661144ad740fc92
Sebastian Triemer <sebastian.triemer@outlook.de>
2023-12-14 09:04:15 +0100

    added parameters for the neuralnetwork

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,10 +2,18 @@ import numpy
 import scipy.special
 import matplotlib.pyplot
 
+input_nodes = 3
+output_nodes = 3
+hidden_nodes = 3
+learning_rate = 0.5
+
 class neuralNetwork():
 
-    def __init__(self):
-        return
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate
 
     def train(self):
         print("I'am training")
@@ -15,6 +23,6 @@ class neuralNetwork():
         print("I ask...")
 
 
-neuralNetwork = neuralNetwork()
+neuralNetwork = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 neuralNetwork.train()
 neuralNetwork.query()
