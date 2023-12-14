dc54c79f861f7a755dad2b36f66fcf5870813ea5
Sebastian Triemer <sebastian.triemer@outlook.de>
2023-12-14 09:36:09 +0100

    added weights

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -15,6 +15,14 @@ class neuralNetwork():
         self.onodes = outputnodes
         self.lr = learningrate
 
+        self.wHiddenOutput = numpy.random.rand(self.onodes, self.hnodes) -0.5
+        self.wInputHidden = numpy.random.rand(self.hnodes, self.inodes) -0.5
+
+        print("Weight-Input-Hidden: ")
+        print(self.wInputHidden)
+        print("Weight-Hidden-Output: ")
+        print(self.wHiddenOutput)
+
     def train(self):
         print("I'am training")
     
@@ -26,3 +34,6 @@ class neuralNetwork():
 neuralNetwork = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 neuralNetwork.train()
 neuralNetwork.query()
+
+wHiddenOutput = numpy.random.rand(3,3) - 0.5
+print (wHiddenOutput)
